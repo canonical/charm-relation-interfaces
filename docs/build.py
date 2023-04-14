8676ce68c502a15ba41fb91eaaebb56a2a28fdc5
Pietro Pasotti <starfire.daemon@gmail.com>
2023-04-14 12:43:57 +0200

    build using interface tester collector; cleaned tests

--- docs/build.py
+++ docs/build.py
@@ -1,48 +1,17 @@
 # Copyright 2023 Canonical
 # See LICENSE file for licensing details.
-
-import importlib
 import logging
-import sys
-import types
 from pathlib import Path
 from typing import Type
 
 import pydantic
+from interface_tester.collector import get_schemas
 
 logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
 
 ROOT = Path(__file__).parent.parent
 JSON_SCHEMAS_ROOT = ROOT / "docs" / "json_schemas"
-
-
-def get_schema_from_module(module: object, name: str) -> Type[pydantic.BaseModel]:
-    """Tries to get ``name`` from ``module``, expecting to find a pydantic.BaseModel."""
-    schema_cls = getattr(module, name, None)
-    if not schema_cls:
-        raise NameError(name)
-    if not issubclass(schema_cls, pydantic.BaseModel):
-        raise TypeError(type(schema_cls))
-    return schema_cls
-
-
-def load_schema_module(schema_path: Path) -> types.ModuleType:
-    """Import the schema.py file as a python module."""
-    # so we can import without tricks
-    sys.path.append(str(schema_path.parent))
-
-    # strip .py
-    module_name = str(schema_path.with_suffix("").name)
-    try:
-        module = importlib.import_module(module_name)
-    except ImportError:
-        raise
-    finally:
-        # cleanup, just in case
-        sys.path.remove(str(schema_path.parent))
-
-    return module
 
 
 def dump_json_schema(schema_cls: Type[pydantic.BaseModel], output_location: Path):
@@ -63,29 +32,7 @@ def build_schemas_from_source(
     output_location: Path = JSON_SCHEMAS_ROOT,
 ):
     """Load the schemas from schema.py, dump them to docs/json_schemas/<role>.json."""
-
-    try:
-        module = load_schema_module(schema_path)
-    except ImportError as e:
-        logger.error(f"Failed to load module {schema_path}: {e}")
-        return
-
-    for role, name in (("provider", "ProviderSchema"), ("requirer", "RequirerSchema")):
-        try:
-            schema_cls = get_schema_from_module(module, name)
-        except NameError:
-            logger.warning(
-                f"Failed to load {name} from {schema_path}: "
-                f"schema not defined for role: {role}."
-            )
-            continue
-        except TypeError as e:
-            logger.error(
-                f"Found object called {name!r} in {schema_path}; "
-                f"expecting a DataBagSchema subclass, not {e.args[0]!r}."
-            )
-            continue
-
+    for role, schema_cls in get_schemas(schema_path).items():
         # if the schema is at /path/to/interfaces/foo/v3/schema.py,
         # we take [foo, v3]
         # the output path becomes /path/to/output_location/foo/v3/{role}.json`
