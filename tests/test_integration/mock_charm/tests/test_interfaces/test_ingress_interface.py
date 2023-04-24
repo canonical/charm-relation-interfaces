af561a60131ff6226bf3a8f6a765b8e94db0e012
Pietro Pasotti <starfire.daemon@gmail.com>
2023-04-24 16:27:39 +0200

    version fixed

--- tests/test_integration/mock_charm/tests/test_interfaces/test_ingress_interface.py
+++ tests/test_integration/mock_charm/tests/test_interfaces/test_ingress_interface.py
@@ -4,20 +4,29 @@ from unittest.mock import patch
 
 import pytest
 from interface_tester import InterfaceTester
+from interface_tester.errors import InterfaceTestsFailed, NoTestsRun
 
 
 def test_ingress_interface(interface_tester: InterfaceTester):
-    interface_tester.configure(
-        interface_name="ingress",
-    )
+    interface_tester.configure(interface_name="ingress", interface_version=1)
     interface_tester.run()
 
 
-@pytest.mark.xfail  # replace with pytest.raises after itester.run() raises a proper runtimeerror.
 def test_ingress_interface_fails(interface_tester: InterfaceTester):
     # if we disable using the ingress lib, this test would fail schema validation.
     with patch("charm.MyCharm.use_ingress", False):
+        interface_tester.configure(interface_name="ingress", interface_version=1)
+        with pytest.raises(InterfaceTestsFailed):
+            interface_tester.run()
+
+
+def test_ingress_per_unit_interface_fails(interface_tester: InterfaceTester):
+    # if we disable using the ingress lib, this test would fail schema validation.
+    with patch("charm.MyCharm.use_ingress", False):
         interface_tester.configure(
-            interface_name="ingress",
+            # nonexistent interface
+            interface_name="foobarbaz",
+            interface_version=0,
         )
-        interface_tester.run()
+        with pytest.raises(NoTestsRun):
+            interface_tester.run()
