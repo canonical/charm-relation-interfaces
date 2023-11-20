b0b2ed2c71aef1b79020b3012ed3e36f43af4cb1
arturo-seijas <arturo.seijas@canonical.com>
2023-11-20 12:36:44 +0100

    Provider tests

--- interfaces/saml/v0/interface_tests/provider_tests.py
+++ interfaces/saml/v0/interface_tests/provider_tests.py
@@ -1,14 +1,15 @@
 # Copyright 2023 Canonical
 # See LICENSE file for licensing details.
-import yaml
-from interface_tester.interface_test import SchemaConfig, interface_test_case
+from interface_tester.interface_test import Tester
 from scenario import Relation, State
 
 
-@interface_test_case(
-    event='saml-relation-created',
-    role='provider'
-)
-def test_data_published_on_created(output_state: State):
-    return  # schema validation is enough for now
-
+def test_data_published_on_created():
+    t = Tester(State(
+        relations=[Relation(
+            endpoint='saml',
+            interface='saml'
+        )]
+    ))
+    t.run("saml-relation-created")
+    t.assert_schema_valid()
