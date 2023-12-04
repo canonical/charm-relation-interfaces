035e8a158aa73fcc7876133da49b75eea112ad28
arturo-seijas <arturo.seijas@canonical.com>
2023-12-04 13:14:30 +0100

    Add smtp interface

---
+++ interfaces/smtp/README.md
@@ -0,0 +1,57 @@
+# `smtp`
+
+## Overview
+
+This relation interface describes the expected behavior between of any charm claiming to be able to interface with a SMTP Provider and the SMTP Provider itself.
+
+## Usage
+
+In most cases, this will be accomplished using the [smtp library](https://github.com/canonical/smtp-integrator-operator/blob/main/lib/charms/smtp_integrator/v0/smtp.py), although charm developers are free to provide alternative libraries as long as they fulfil the behavioural and schematic requirements described in this document.
+
+## Direction
+
+The `smtp` interface implements a provider/requirer pattern.
+The requirer is a charm that requires SMTP details to connect to an SMTP server, and the provider is a charm holding those details.
+
+```mermaid
+flowchart TD
+    Provider -- host, port, user, password_id, auth_type, transport_security, domain --> Requirer
+```
+
+## Behavior
+
+The requirer and the provider must adhere to a certain set of criteria to be considered compatible with the interface.
+
+### Provider
+
+- Is expected to provide the host in the relation databag.
+- Is expected to provide the port in the relation databag.
+- Is expected to provide the auth_type in the relation databag.
+- Is expected to provide the transport_security in the relation databag.
+
+### Requirer
+
+- Is not expected to provide anything
+
+## Relation Data
+
+### Provider
+
+[\[JSON Schema\]](./schemas/provider.json)
+
+Provider provides the SMTP configuration. It should be placed in the **application** databag.
+
+#### Example
+
+```yaml
+related-units: {}
+application_data: {
+  "host": "example.smtp",
+  "port": "25",
+  "user": "example_user",
+  "password_id": "01548499c9233d4612352c989162d940f6a9e6f6d5cc058dfcf66f51575e09c2",
+  "auth_type": "plain",
+  "transport_security": "tls",
+  "domain": "domain",
+}
+```
---
+++ interfaces/smtp/schema.py
@@ -0,0 +1,102 @@
+# Copyright 2023 Canonical
+# See LICENSE file for licensing details.
+"""This file defines the schema for the provider side of the saml interface.
+
+It exposes one interfaces.schema_base.DataBagSchema subclass called:
+- ProviderSchema
+
+Examples:
+    ProviderSchema:
+        unit: <empty>
+        app: {"smtp":
+                 {
+                    "host": "example.smtp",
+                    "port": "25",
+                    "user": "example_user",
+                    "password_id": "01548499c9233d4612352c989162d940f6a9e6f6d5cc058dfcf66f51575e09c2",
+                    "auth_type": "plain",
+                    "transport_security": "tls",
+                    "domain": "domain",
+                }
+             }
+"""
+from interface_tester.schema_base import DataBagSchema
+from pydantic import AnyHttpUrl, BaseModel, Field
+from typing import Optional
+
+
+class TransportSecurity(str, Enum):
+    """Represent the transport security values.
+
+    Attributes:
+        NONE: none
+        STARTTLS: starttls
+        TLS: tls
+    """
+
+    NONE = "none"
+    STARTTLS = "starttls"
+    TLS = "tls"
+
+
+class AuthType(str, Enum):
+    """Represent the auth type values.
+
+    Attributes:
+        NONE: none
+        NOT_PROVIDED: not_provided
+        PLAIN: plain
+    """
+
+    NONE = "none"
+    NOT_PROVIDED = "not_provided"
+    PLAIN = "plain"
+
+class SmtpProviderData(BaseModel):
+    host: str = Field(
+        ...,
+        min_length=1,
+        description="SMTP host.",
+        title="Host",
+        examples=["example.smtp"],
+    )
+    port: int = Field(None,
+        ge=1,
+        le=65536,
+        description="SMTP port.",
+        title="Port",
+        examples=[25],
+    )
+    user: Optional[str] = Field(
+        description="SMTP user.",
+        title="User",
+        examples=["some_user"],
+    )
+    password_id: Optional[str] = Field(
+        description="Secret ID for the SMTP password.",
+        title="Password id",
+        examples=["01548499c9233d4612352c989162d940f6a9e6f6d5cc058dfcf66f51575e09c2"],
+    )
+    auth_type: AuthType = Field(
+        description="The type used to authenticate with the SMTP relay.",
+        title="Auth type",
+        examples=[AuthType.NONE],
+    )
+    transport_security: TransportSecurity = Field(
+        description="The security protocol to use for the outgoing SMTP relay.",
+        title="Transport security",
+        examples=[TransportSecurity.NONE],
+    )
+    domain: Optional[str] = Field(
+        description="The domain used by the sent emails from SMTP relay.",
+        title="Domain",
+        examples=["domain"],
+    )
+
+
+class ProviderSchema(DataBagSchema):
+    """Provider schema for SMTP."""
+    app: SmtpProviderData
+
+class RequirerSchema(DataBagSchema):
+    """Requirer schema for SMTP."""
