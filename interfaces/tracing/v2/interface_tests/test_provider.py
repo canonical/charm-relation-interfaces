e0fea1f4ae9e8aea9c8d9360fe1c46250098affb
Pietro Pasotti <starfire.daemon@gmail.com>
2024-01-31 14:34:02 +0100

    removed backwards compatibility checks

--- interfaces/tracing/v2/interface_tests/test_provider.py
+++ interfaces/tracing/v2/interface_tests/test_provider.py
@@ -1,40 +1,9 @@
 # Copyright 2023 Canonical
 # See LICENSE file for licensing details.
 import json
-from typing import List, Any
 
-from interface_tester import DataBagSchema
 from interface_tester.interface_test import Tester
-from pydantic import BaseModel, Json
 from scenario import State, Relation
-
-
-class LegacyTracingProviderData(BaseModel):
-    host: str
-    ingesters: Json[List[Any]]
-
-
-class LegacyProviderSchema(DataBagSchema):
-    """Provider schema for Tracing."""
-    app: LegacyTracingProviderData
-
-
-def test_legacy_response_on_created_no_data():
-    tester = Tester()
-    tester.run('tracing-relation-created')
-    tester.assert_schema_valid(LegacyProviderSchema)
-
-
-def test_legacy_response_on_joined_no_data():
-    tester = Tester()
-    tester.run('tracing-relation-joined')
-    tester.assert_schema_valid(LegacyProviderSchema)
-
-
-def test_legacy_response_on_changed_no_data():
-    tester = Tester()
-    tester.run('tracing-relation-changed')
-    tester.assert_schema_valid(LegacyProviderSchema)
 
 
 def test_no_response_on_bad_data():
