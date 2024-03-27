7e9ebc74fe00b42ebd4d73f4ffcd16edf2aefdbb
Arturo Seijas <arturo.seijas@canonical.com>
2024-03-27 10:21:26 +0100

    Add dns entries and generate schema

--- interfaces/dns_record/v0/schema.py
+++ interfaces/dns_record/v0/schema.py
@@ -20,7 +20,22 @@ Examples:
               "username": "user2",
               "password": "secret:123213123123123123123"
             }
-          ]
+          ],
+          dns-entries: ‘[
+            {
+              "domain": "canonical.com",
+              "host_label": "admin",
+              "ttl": 600,
+              "record_class": "IN",
+              "record_type": "A",
+              "record_data": "91.189.91.48"
+            },{
+              "domain": "canonical.com",
+              "host_label": "www",
+              "record_data": "91.189.91.47"
+            }
+        ]’
+
         }
 
     ProviderSchema:
@@ -36,13 +51,28 @@ Examples:
               "domain": "staging.ubuntu.com",
               "status": "approved"
             }
-          ]
+          ],
+          dns-entries: [
+            {
+              "domain": "cloud.canonical.com",
+              "host_label": "admin",
+              "record_type": "A",
+              "status": "denied",
+              "status_description": "incorrect username & password"
+            },
+            {
+              "domain": "canonical.com",
+              "host_label": "www",
+              "status": "approved"
+            }
+        ]
+
         }
 """
 
 from enum import Enum
 from typing import List, Optional
-from pydantic import AnyHttpUrl, BaseModel, Field
+from pydantic import IPvAnyAddress, BaseModel, Field
 
 from interface_tester.schema_base import DataBagSchema
 
@@ -54,7 +84,37 @@ class Status(str, Enum):
     DENIED = "denied"
 
 
-class ProviderDomains:
+class RecordType(str, Enum):
+    """Represent the DNS record types."""
+
+    A = "A"
+    AAAA = "AAAA"
+    CNAME = "CNAME"
+    MX = "MX"
+    DKIM = "DKIM"
+    SPF = "SPF"
+    DMARC = "DMARC"
+    TXT = "TXT"
+    CAA = "CAA"
+    SRV = "SRV"
+    SVCB = "SVCB"
+    HTTPS = "HTTPS"
+    PTR = "PTR"
+    SOA = "SOA"
+    NS = "NS"
+    DS = "DS"
+    DNSKEY = "DNSKEY"
+
+
+class RecordClass(str, Enum):
+    """Represent the DNS record classes."""
+
+    IN = "IN"
+    CH = "CH"
+    HS = "HS"
+
+
+class ProviderDomains(BaseModel):
     domain: str = Field(
         min_length=1,
         name="Domain",
@@ -73,14 +133,47 @@ class ProviderDomains:
     )
 
 
-class DnsRecordProvider(BaseModel):
-    """List statuses for the DNS records informed by the requirer."""
-    dns_domains: List[ProviderDomains] = Field(
-        description="List statuses for the DNS records informed by the requirer."
+class ProviderEntries(BaseModel):
+    domain: str = Field(
+        min_length=1,
+        name="Domain",
+        description="Domain name requested.",
+        examples=["cloud.canonical.com", "staging.ubuntu.com"]
+    )
+    host_label: str = Field(
+        min_length=1,
+        name="Host label",
+        status_description="Host label",
+        examples=["admin", "www"]
+    )
+    record_type: Optional[RecordType] =Field(
+        name="Record type label",
+        status_description="The DNS record type.",
+        examples=[RecordType.A, RecordType.CNAME]
+    )
+    status: Status = Field(
+        name="Status",
+        description="Status for the domain request.",
+        examples=[Status.APPROVED, Status.DENIED]
+    )
+    status_description: Optional[str] = Field(
+        name="Status description",
+        description="Status description.",
+        examples=["incorrect username and password"]
     )
 
 
-class RequirerDomains:
+class DnsRecordProvider(BaseModel):
+    """List statuses for the DNS records informed by the requirer."""
+    dns_domains: Optional[List[ProviderDomains]] = Field(
+        description="List statuses for the domains requested by the requirer."
+    )
+    dns_entries: Optional[List[ProviderEntries]] = Field(
+        description="List of statuses for the DNS records requested by the requirer."
+    )
+
+
+class RequirerDomains(BaseModel):
     domain: str = Field(
         min_length=1,
         name="Domain",
@@ -99,9 +192,48 @@ class RequirerDomains:
     )
 
 
+class RequirerEntries(BaseModel):
+    domain: str = Field(
+        min_length=1,
+        name="Domain",
+        description="Domain name requested.",
+        examples=["cloud.canonical.com", "staging.ubuntu.com"]
+    )
+    host_label: str = Field(
+        min_length=1,
+        name="Host label",
+        status_description="Host label",
+        examples=["admin", "www"]
+    )
+    ttl: Optional[int] = Field(
+        name="TTL",
+        status_description="The DNS time to live",
+        examples=[600, 1200]
+    )
+    record_class: Optional[RecordClass] = Field(
+        name="Record class",
+        status_description="The DNS record class.",
+        examples=[RecordClass.IN, RecordClass.HS]
+    )
+    record_type: Optional[RecordType] =Field(
+        name="Record type",
+        status_description="The DNS record type.",
+        examples=[RecordType.A, RecordType.CNAME]
+    )
+    record_data: IPvAnyAddress = Field(
+        min_length=1,
+        name="Record data",
+        description="The DNS record value.",
+        examples=["91.189.91.47", "91.189.91.48"]
+    )
+
+
 class DnsRecordRequirer(BaseModel):
     """List of domains for the provider to manage."""
-    dns_domains: List[RequirerDomains] = Field(
+    dns_domains: Optional[List[RequirerDomains]] = Field(
+        description="List of domains for the provider to manage."
+    )
+    dns_entries: Optional[List[RequirerEntries]] = Field(
         description="List of DNS records for the provider to manage."
     )
 
