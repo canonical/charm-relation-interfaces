07e0d40f1cdbef48ebd929d0508dfaf5fcd821d2
Alex Batisse <alex.batisse@canonical.com>
2024-04-25 15:38:33 +0200

    WIP: Add minimal provider test

--- interfaces/zookeeper_client/v0/interface_tests/test_provider.py
+++ interfaces/zookeeper_client/v0/interface_tests/test_provider.py
@@ -1,1 +1,8 @@
-# TODO: add here interface tests for the provider side
+from scenario import State
+from interface_tester import Tester
+
+def test_data_published_on_joined():
+    t = Tester(State())
+    t.run("database-relation-joined")
+    t.assert_relation_data_empty()
+
