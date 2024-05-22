44502aa66b18580895283027e0d65d9e3c337754
michael <michael.dmitry98@gmail.com>
2024-05-22 12:04:26 +0300

    update tracing relation interfaces

---
+++ interfaces/tracing/v2/schema.py
@@ -0,0 +1,90 @@
+# Copyright 2024 Canonical
+# See LICENSE file for licensing details.
+"""This file defines the schemas for the provider and requirer sides of the tracing interface.
+
+It exposes two interfaces.schema_base.DataBagSchema subclasses called:
+- ProviderSchema
+- RequirerSchema
+
+Examples:
+    RequirerSchema:
+        unit_data: <empty>
+        application_data:
+          receivers:
+            - otlp_grpc
+            - otlp_http
+
+    ProviderSchema:
+        # unit_data: <empty>
+        application_data:            
+          receivers:
+            - protocol: 
+                name: otlp_http
+                type: http
+              url: http://traefik_address:2331
+            - protocol: 
+                name: otlp_grpc
+                type: grpc
+              url: traefik_address:2331
+
+"""
+from typing import List
+import enum
+
+from interface_tester.schema_base import DataBagSchema
+from pydantic import BaseModel, Json, Field
+
+
+class TransportProtocolType(str, enum.Enum):
+    """Receiver Type."""
+    http = "http"
+    grpc = "grpc"
+    
+class ProtocolType(BaseModel):
+    """Protocol Type."""
+    name: str = Field(
+      ...,
+        description="Receiver protocol name. What protocols are supported (and what they are called) "
+                    "may differ per provider.",
+        examples=["otlp_grpc", "otlp_http", "tempo_http", "jaeger_thrift_compact"])
+    type: TransportProtocolType = Field(
+        ...,
+        description="The transport protocol used by this receiver.",
+        examples=["http", "grpc"])
+    
+class Receiver(BaseModel):
+    """Specification of an active receiver."""
+    
+    
+    protocol: ProtocolType = Field(
+        ...,
+        description="Receiver protocol name and type.")
+    url: str = Field(
+      ...,
+      description="URL at which the receiver is reachable. If there's an ingress, it would be the external URL. Otherwise, it would be the service's fqdn or internal IP",
+      examples=["http://traefik_address:2331",
+                "https://traefik_address:2331",
+                "http://tempo_public_ip:2331",
+                "https://tempo_public_ip:2331",
+                "tempo_public_ip:2331"]
+    )
+
+
+class TracingProviderData(BaseModel):
+    host: str = Field(..., description="Hostname of the tracing server.", examples=["example.com"])
+    receivers: Json[List[Receiver]] = Field(
+        ...,
+        description="List of the receivers that this server has enabled, and their ports.")
+
+
+class ProviderSchema(DataBagSchema):
+    """Provider schema for Tracing."""
+    app: TracingProviderData
+
+
+class RequirerSchema(DataBagSchema):
+    """Requirer schema for Tracing."""
+    protocols: Json[List[str]] = Field(
+        ...,
+        description="List of protocols that the requirer wishes to use."
+    )
