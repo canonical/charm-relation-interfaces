499922f09dc7e863430a8c25949a520d9c51cc7b
michael <michael.dmitry98@gmail.com>
2024-05-29 17:34:40 +0300

    format and rephrase comment

--- interfaces/tracing/v2/schema.py
+++ interfaces/tracing/v2/schema.py
@@ -37,58 +37,68 @@ from pydantic import BaseModel, Json, Field, ConfigDict
 
 class TransportProtocolType(str, enum.Enum):
     """Receiver Type."""
+
     http = "http"
     grpc = "grpc"
-    
+
+
 class ProtocolType(BaseModel):
     """Protocol Type."""
+
     model_config = ConfigDict(
         # Allow serializing enum values.
         use_enum_values=True
     )
     """Pydantic config."""
-    
+
     name: str = Field(
-      ...,
+        ...,
         description="Receiver protocol name. What protocols are supported (and what they are called) "
-                    "may differ per provider.",
-        examples=["otlp_grpc", "otlp_http", "tempo_http", "jaeger_thrift_compact"])
+        "may differ per provider.",
+        examples=["otlp_grpc", "otlp_http", "tempo_http", "jaeger_thrift_compact"],
+    )
     type: TransportProtocolType = Field(
         ...,
         description="The transport protocol used by this receiver.",
-        examples=["http", "grpc"])
-    
+        examples=["http", "grpc"],
+    )
+
+
 class Receiver(BaseModel):
     """Specification of an active receiver."""
-    
-    protocol: ProtocolType = Field(
-        ...,
-        description="Receiver protocol name and type.")
+
+    protocol: ProtocolType = Field(..., description="Receiver protocol name and type.")
     url: str = Field(
-      ...,
-      description="URL at which the receiver is reachable. If there's an ingress, it would be the external URL. Otherwise, it would be the service's fqdn or internal IP",
-      examples=["http://traefik_address:2331",
-                "https://traefik_address:2331",
-                "http://tempo_public_ip:2331",
-                "https://tempo_public_ip:2331",
-                "tempo_public_ip:2331"]
+        ...,
+        description="""URL at which the receiver is reachable. If there's an ingress, it would be the external URL.
+        Otherwise, it would be the service's fqdn or internal IP.
+        If the protocol type is grpc, the url will not contain a scheme.""",
+        examples=[
+            "http://traefik_address:2331",
+            "https://traefik_address:2331",
+            "http://tempo_public_ip:2331",
+            "https://tempo_public_ip:2331",
+            "tempo_public_ip:2331",
+        ],
     )
 
 
 class TracingProviderData(BaseModel):
     receivers: Json[List[Receiver]] = Field(
         ...,
-        description="A list of enabled receivers in the form of the protocol they use and their resolvable server url.")
+        description="A list of enabled receivers in the form of the protocol they use and their resolvable server url.",
+    )
 
 
 class ProviderSchema(DataBagSchema):
     """Provider schema for Tracing."""
+
     app: TracingProviderData
 
 
 class RequirerSchema(DataBagSchema):
     """Requirer schema for Tracing."""
+
     receivers: Json[List[str]] = Field(
-        ...,
-        description="List of protocols that the requirer wishes to use."
+        ..., description="List of protocols that the requirer wishes to use."
     )
