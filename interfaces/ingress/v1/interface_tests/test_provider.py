2455ede78dd6e7ed80acea759892a75b0518686c
Tiexin Guo <guotiexin@gmail.com>
2024-06-20 14:10:24 +0800

    chore: tox fmt

--- interfaces/ingress/v1/interface_tests/test_provider.py
+++ interfaces/ingress/v1/interface_tests/test_provider.py
@@ -8,13 +8,14 @@ from scenario import State, Relation
 
 def test_no_data_on_created():
     t = Tester(
-        State(leader=True,
+        State(
+            leader=True,
             relations=[
                 Relation(
                     endpoint="ingress",
                     interface="ingress",
                 )
-            ]
+            ],
         )
     )
     state_out = t.run("ingress-relation-created")
@@ -24,13 +25,14 @@ def test_no_data_on_created():
 def test_no_data_on_joined():
     # nothing happens on joined: databags are empty
     t = Tester(
-        State(leader=True,
+        State(
+            leader=True,
             relations=[
                 Relation(
                     endpoint="ingress",
                     interface="ingress",
                 )
-            ]
+            ],
         )
     )
     state_out = t.run("ingress-relation-joined")
@@ -39,8 +41,14 @@ def test_no_data_on_joined():
 
 def test_data_published_on_changed_remote_valid():
     ingress = Relation(
-        endpoint='ingress', interface='ingress',
-        remote_app_data={'host': '"0.0.0.42"', 'model': '"bar"', 'name': '"remote/0"', 'port': '42'}
+        endpoint="ingress",
+        interface="ingress",
+        remote_app_data={
+            "host": '"0.0.0.42"',
+            "model": '"bar"',
+            "name": '"remote/0"',
+            "port": "42",
+        },
     )
     t = Tester(State(leader=True, relations=[ingress]))
     state_out = t.run(ingress.changed_event)
@@ -50,17 +58,16 @@ def test_data_published_on_changed_remote_valid():
 def test_no_data_published_on_changed_remote_invalid():
     # on changed, if the remote side has sent INvalid data: local side didn't publish anything either.
     t = Tester(
-        State(leader=True,
-              relations=[Relation(
-                  endpoint='ingress',
-                  interface='ingress',
-                  remote_app_data={
-                      'host': '0.0.0.42',
-                      'bubble': "10",
-                      'rubble': "foo"
-                  }
-              )]
-              )
+        State(
+            leader=True,
+            relations=[
+                Relation(
+                    endpoint="ingress",
+                    interface="ingress",
+                    remote_app_data={"host": "0.0.0.42", "bubble": "10", "rubble": "foo"},
+                )
+            ],
+        )
     )
     state_out = t.run("ingress-relation-changed")
     t.assert_relation_data_empty()
