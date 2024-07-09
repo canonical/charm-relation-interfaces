360bee18f53ce5191ebce229edaa9bff484e7db1
kayra1 <gemalmazkayra@gmail.com>
2024-07-09 12:07:58 +0300

    app databag instead of unit

--- interfaces/certificate_transfer/v1/schema.py
+++ interfaces/certificate_transfer/v1/schema.py
@@ -15,20 +15,23 @@ Examples:
         unit: <empty>
         app:  <empty>
 """
+
 from pydantic import BaseModel, Field
 from typing import Set
 from interface_tester.schema_base import DataBagSchema
 
+
 class CertificateTransferProviderAppData(BaseModel):
-    certificates: Set[str]= Field(
+    certificates: Set[str] = Field(
         description="The set of certificates that will be transferred to a requirer"
     )
 
+
 class ProviderSchema(DataBagSchema):
     """Provider schema for certificate_transfer."""
-    unit: CertificateTransferProviderAppData
+
+    app: CertificateTransferProviderAppData
 
 
 class RequirerSchema(DataBagSchema):
     """Requirer schema for certificate_transfer."""
-
