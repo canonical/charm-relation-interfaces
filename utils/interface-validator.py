0e9f83048df22915268cde292c9dc5700a02de4f
Tiexin Guo <guotiexin@gmail.com>
2024-08-07 16:30:37 +0800

    chore: fix validator

--- utils/interface-validator.py
+++ utils/interface-validator.py
@@ -44,6 +44,7 @@ class InterfaceModel(BaseModel):
     status: StatusEnum
     requirers: List[CharmEntry]
     providers: List[CharmEntry]
+    owners: Optional[List[str]] = []
 
 class MatchError(Exception):
     """Error raised when the location of an interface.yaml spec file is inconsistent with its contents."""
