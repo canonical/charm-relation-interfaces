a3d8935fc9687513f84ba86f0f0d300282b0af8e
Patricia Reinoso <patricia.reinoso@canonical.com>
2024-08-27 15:10:59 +0200

    fix sdcore_config interface tests

--- interfaces/sdcore_config/v0/interface_tests/test_provider.py
+++ interfaces/sdcore_config/v0/interface_tests/test_provider.py
@@ -1,8 +1,15 @@
 # Copyright 2024 Canonical
 # See LICENSE file for licensing details.
 
-from interface_tester.interface_test import Tester
+from interface_tester import Tester
 from scenario import State, Relation
+import json
+
+
+def test_no_data_on_created():
+    t = Tester()
+    t.run("sdcore-config-relation-created")
+    t.assert_relation_data_empty()
 
 
 def test_data_published_on_created():
@@ -16,9 +23,8 @@ def test_data_published_on_created():
             ],
         )
     )
-    state_out: State = t.run("sdcore-config-relation-created")
-    t.assert_schema_valid()
-    assert state_out.unit_status.name == 'active'
+    t.run("sdcore-config-relation-created")
+    t.assert_relation_data_empty()
 
 
 def test_data_published_on_joined():
@@ -32,39 +38,5 @@ def test_data_published_on_joined():
             ],
         )
     )
-    state_out: State = t.run("sdcore-config-relation-joined")
+    t.run("sdcore-config-relation-joined")
     t.assert_schema_valid()
-    assert state_out.unit_status.name == 'active'
-
-
-def test_data_published_on_changed():
-    t = Tester(
-        State(
-            relations=[
-                Relation(
-                    endpoint="sdcore_config",
-                    interface="sdcore_config",
-                )
-            ],
-        )
-    )
-    state_out: State = t.run("sdcore-config-relation-changed")
-    t.assert_schema_valid()
-    assert state_out.unit_status.name == 'active'
-
-
-def test_no_data_on_broken():
-    t = Tester(
-        State(
-            relations=[
-                Relation(
-                    endpoint="sdcore_config",
-                    interface="sdcore_config",
-                )
-            ],
-        )
-    )
-    state_out: State = t.run("sdcore-config-relation-broken")
-    t.assert_relation_data_empty()
-    assert state_out.unit_status.name == 'active'
-
--- interfaces/sdcore_config/v0/interface_tests/test_requirer.py
+++ interfaces/sdcore_config/v0/interface_tests/test_requirer.py
@@ -5,33 +5,17 @@ from interface_tester import Tester
 from scenario import Relation, State
 
 
-def test_state_active_on_joined():
+def test_no_data_on_joined():
     t = Tester(
         State(
             relations=[
                 Relation(
                     endpoint="sdcore_config",
                     interface="sdcore_config",
+                    remote_app_data={"webui_url": "some_url:123"},
                 )
             ],
         )
     )
-    state_out: State = t.run('sdcore-config-relation-joined')
-    assert state_out.unit_status.name == 'active'
-
-
-def test_state_blocked_on_broken():
-    t = Tester(
-        State(
-            relations=[
-                Relation(
-                    endpoint="sdcore_config",
-                    interface="sdcore_config",
-                )
-            ],
-        )
-    )
-    state_out: State = t.run('sdcore-config-relation-broken')
-    assert state_out.unit_status.name == 'blocked'
-
-
+    t.run('sdcore-config-relation-joined')
+    t.assert_relation_data_empty()
