46c4f94e142ce88d8a498a51107975a47cdb747e
Dario Faccin <dario.faccin@canonical.com>
2024-11-08 15:38:26 +0100

    chore: update relation according to new spec
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- interfaces/fiveg_core_gnb/v0/schema.py
+++ interfaces/fiveg_core_gnb/v0/schema.py
@@ -8,11 +8,15 @@ Examples:
     ProviderSchema:
         unit: <empty>
         app: {
-            "mcc": "001",
-            "mnc": "01",
             "tac": 1,
-            "sst": 1,
-            "sd": 1,
+            "plmns": [
+                {
+                    "mcc": "001",
+                    "mnc": "01",
+                    "sst": 1,
+                    "sd": 1,
+                }
+            ],
         }
     RequirerSchema:
         unit: <empty>
@@ -26,36 +30,13 @@ from pydantic import BaseModel, Field
 
 
 class FivegCoreGnbProviderAppData(BaseModel):
-    mcc: str = Field(
-        description="Mobile Country Code",
-        examples=["001"],
-        min_length=3,
-        max_length=3,
-    )
-    mnc: str = Field(
-        description="Mobile Network Code",
-        examples=["01"],
-        min_length=2,
-        max_length=3,
-    )
     tac: int = Field(
         description="Tracking Area Code",
         examples=[1],
         ge=1,
         le=16777215,
     )
-    sst: int = Field(
-        description="Slice Service Type",
-        examples=[1],
-        ge=1,
-        le=4,
-    )
-    sd: int = Field(
-        description="Slice Differentiator",
-        examples=[1],
-        ge=1,
-        le=16777215,
-    )
+    plmns: list[dict]
 
 
 class FivegCoreGnbRequirerAppData(BaseModel):
