e814e41e1e2eecde85838162377b8a0467078e97
Pietro Pasotti <starfire.daemon@gmail.com>
2024-12-04 13:42:28 +0100

    simplify test

--- interfaces/grafana_datasource_exchange/v0/interface_tests/test_provider.py
+++ interfaces/grafana_datasource_exchange/v0/interface_tests/test_provider.py
@@ -5,20 +5,7 @@ from scenario import State, Relation
 
 
 def test_datasource_exchange():
-    # GIVEN the grafana_datasource interface has shared one or more sourdce UID
-    source = Relation(
-        endpoint='grafana-source',
-        interface='grafana_datasource',
-        remote_app_name='foo',
-        local_app_data={"grafana_source_data": json.dumps(
-            {"model": "somemodel", "model_uuid": "0000-0000-0000-0042", "application": "myapp",
-             "type": "prometheus", })},
-        local_unit_data={"grafana_source_host": "somehost:80"},
-        remote_app_data={"datasource_uids": json.dumps({
-            "foo/0": "myuid0",
-            "foo/4": "myuid1"
-        })}
-    )
+    # GIVEN the grafana_datasource interface has shared one or more source UIDs
     source_exchange = Relation(
         endpoint='grafana-source-exchange',
         interface='grafana_datasource_exchange',
@@ -26,11 +13,10 @@ def test_datasource_exchange():
     )
     tester = Tester(state_in=State(
         relations=[
-            source,
             source_exchange
         ]
     ))
     # WHEN the provider processes any relation event
-    tester.run('grafana-source-relation-changed')
-    # THEN the provider publishes valid datasource-exchange data
+    tester.run('grafana-source-exchange-relation-changed')
+    # THEN the provider publishes valid data
     tester.assert_schema_valid()
