6e63f289272fe296c577ee39f0e0f05ef12b2863
Bikalpa Dhakal <bikalpa.dhakal@canonical.com>
2025-01-31 14:33:41 +0000

    Remove requirer interface tests

--- interfaces/azure_storage/v0/interface_tests/test_provider.py
+++ interfaces/azure_storage/v0/interface_tests/test_provider.py
@@ -1,7 +1,6 @@
 from interface_tester import Tester
 from scenario import State, Relation
 
-
 def test_nothing_happens_if_remote_empty():
     # GIVEN that the remote end has not published anything on databag
     t = Tester(
@@ -21,3 +20,24 @@ def test_nothing_happens_if_remote_empty():
 
     # THEN no data is published to the (local) databags
     t.assert_relation_data_empty()
+
+
+def test_data_written_happy_path():
+    # GIVEN that the remote end has requested a container in the right format
+    t = Tester(
+        State(
+            leader=True,
+            relations=[
+                Relation(
+                    endpoint="azure-storage-credentials",
+                    interface="azure_storage",
+                    remote_app_data={"container": "my-container"},
+                )
+            ],
+        )
+    )
+    # WHEN this charm receives a relation-changed event
+    state_out = t.run("azure-storage-credentials-relation-changed")
+
+    # THEN the schema is satisfied (this charm published all required fields)
+    t.assert_schema_valid()
--- interfaces/azure_storage/v0/interface_tests/test_requirer.py
+++
@@ -1,7 +0,0 @@
-# from scenario import State
-# from interface_tester import Tester
-
-# def test_data_published_on_joined():
-#     t = Tester(State())
-#     t.run("azure-storage-credentials-relation-joined")
-#     t.assert_relation_data_empty()
