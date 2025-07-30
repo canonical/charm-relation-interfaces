3920628a147bf764f8ba7f9abdf44f8d5b748834
Deezzir <yurii.kondrakov@canonical.com>
2025-07-30 16:22:53 -0400

    fix: remove data_written test

--- interfaces/velero_backup_config/v0/interface_tests/test_provider.py
+++ interfaces/velero_backup_config/v0/interface_tests/test_provider.py
@@ -18,32 +18,3 @@ def test_data_on_changed():
     t = Tester(State(leader=True))
     state_out = t.run("velero-backup-config-relation-changed")
     t.assert_schema_valid()
-
-def test_data_written():
-    # GIVEN that the remote end has requested a container in the right format
-    t = Tester(
-        State(
-            leader=True,
-            relations=[
-                Relation(
-                    endpoint="velero-backup-config",
-                    interface="velero_backup_config",
-                    remote_app_data={
-                        "app": "velero-kubeflow",
-                        "relation_name": "profiles-backup",
-                        "spec": {
-                            "include_namespaces": ["kubeflow"],
-                            "include_resources": ["profiles.kubeflow.org"],
-                            "label_selector": { "app": "kubeflow" },
-                            "ttl": "24h",
-                        },
-                    },
-                )
-            ],
-        )
-    )
-    # WHEN this charm receives a relation-changed event
-    state_out = t.run("velero-backup-config-relation-changed")
-
-    # THEN the schema is satisfied (this charm published all required fields)
-    t.assert_schema_valid()
