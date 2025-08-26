40217e0f9688e06e153eed4e587d67ae916131b5
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-08-26 11:45:30 +0200

    fmt

--- run_matrix.py
+++ run_matrix.py
@@ -89,7 +89,9 @@ def _prepare_repo(
     repo_root_path = root / charm_config.name
 
     # multi-charm repos might have multiple charms in a single repo.
-    if charm_root_cfg := (charm_config.test_setup.get('charm_root') if charm_config.test_setup else None):
+    if charm_root_cfg := (
+        charm_config.test_setup.get("charm_root") if charm_config.test_setup else None
+    ):
         charm_root_path = repo_root_path / charm_root_cfg
         if not charm_root_path.resolve().is_relative_to(repo_root_path):
             raise SetupError(
